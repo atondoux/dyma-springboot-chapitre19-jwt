891c58fa27eae3b2215a00d156ceaad0c91ec498
Aurélien <atondoux@gmail.com>
2025-01-03 20:26:29 +0100

    :sparkles: add jwt

--- src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
+++ src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
@@ -1,6 +1,11 @@
 package com.dyma.tennis.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,14 +14,30 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
+
+    @Value("${jwt.base64-secret}")
+    private String jwtSecret;
 
     @Autowired
     private DymaUserDetailsService dymaUserDetailsService;
@@ -24,6 +45,44 @@ public class SecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder
+                .withSecretKey(getSecretKey())
+                .macAlgorithm(SecurityUtils.JWT_ALGORITHM)
+                .build();
+        return token -> {
+            try {
+                return jwtDecoder.decode(token);
+            } catch (Exception e) {
+                log.error("Could not decode JWT: {}", e.getMessage());
+                throw e;
+            }
+        };
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtSecret).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtils.JWT_ALGORITHM.getName());
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            return jwt.getClaimAsStringList(SecurityUtils.AUTHORITIES_CLAIM_KEY)
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+        return jwtAuthenticationConverter;
     }
 
     @Bean
@@ -65,8 +124,11 @@ public class SecurityConfiguration {
                                 .requestMatchers(HttpMethod.PUT, "/tournaments/**").hasAuthority("ROLE_ADMIN")
                                 .requestMatchers(HttpMethod.DELETE, "/tournaments/**").hasAuthority("ROLE_ADMIN")
                                 .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 );
         return http.build();
     }
-
 }
--- src/main/java/com/dyma/tennis/web/AccountController.java
+++ src/main/java/com/dyma/tennis/web/AccountController.java
@@ -1,23 +1,26 @@
 package com.dyma.tennis.web;
 
+import com.dyma.tennis.model.UserAuthentication;
 import com.dyma.tennis.model.UserCredentials;
+import com.dyma.tennis.security.JwtService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Accounts API")
 @RestController
@@ -25,35 +28,30 @@ import org.springframework.web.bind.annotation.*;
 public class AccountController {
 
     @Autowired
-    private AuthenticationManagerBuilder authenticationManagerBuilder;
+    private AuthenticationManager authenticationManager;
 
-    private final SecurityContextRepository securityContextRepository = new HttpSessionSecurityContextRepository();
+    @Autowired
+    private JwtService jwtService;
 
     private final SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
 
     @Operation(summary = "Authenticates user", description = "Authenticates user")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User is logged in."),
+            @ApiResponse(responseCode = "200", description = "User is logged in.",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserAuthentication.class))}),
             @ApiResponse(responseCode = "403", description = "User credentials are not valid."),
             @ApiResponse(responseCode = "400", description = "Login or password is not provided.")
     })
     @PostMapping("/login")
-    public void login(@RequestBody @Valid UserCredentials credentials, HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<UserAuthentication> login(@RequestBody @Valid UserCredentials credentials) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(credentials.login(), credentials.password());
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        securityContext.setAuthentication(authentication);
-        securityContextRepository.saveContext(securityContext, request, response);
-    }
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
 
-    @Operation(summary = "Logs off authenticated user", description = "Logs off authenticated user")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "User is logged out."),
-            @ApiResponse(responseCode = "403", description = "No user is logged in.")
-    })
-    @GetMapping("/logout")
-    public void logout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
-        securityContextLogoutHandler.logout(request, response, authentication);
+        String jwt = jwtService.createToken(authentication);
+        return new ResponseEntity<>(
+                new UserAuthentication(authentication.getName(), jwt),
+                HttpStatus.OK
+        );
     }
-
 }
